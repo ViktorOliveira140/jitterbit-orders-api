468084b7bfcd4a4d3d57c8ab00cba101375fa475
ViktorOliveira140 <victoroliver140@gmail.com>
2026-03-08 05:22:42 +0100

    chore: upgrade eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+    },
+    rules: {
+      'no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
+];
+
